82500de028b663721c28aa5a2f522a9ac17868eb
mcichorzA <michal.cichorz@atos.net>
2022-09-02 16:55:46 +0200

    Rounding fanspeed + cpu temp testing mode

--- fanspeed.py
+++ fanspeed.py
@@ -5,6 +5,7 @@
 
 import subprocess
 import logging
+import time
 
 FAN_SPEED_MIN = 0x00
 FAN_SPEED_MAX = 0x64
@@ -17,6 +18,12 @@ FAN_SPEED_MAX = 0x64
 LAMBIENT="20"           # Ambient Inlet Air Temp for Silent Running Fan Curve [Less then or Equal to?]
 NAMBIENT="21"           # Ambient Inlet Air Temp for Normal Running Fan Curve [Equal to or Greater then?]
 HAMBIENT="24"           # Ambient Inlet Air Temp for Hot Running Fan Curve [Equal to or Greater then?]
+
+MINIMAL_FAN_SPEED=10
+MAXIMAL_FAN_SPEED=100
+
+MIN_TEMP = 26
+MAX_TEMP = 58
 
 SETFANSPEED_command_part1= "ipmitool raw 0x30 0x30 0x02 0xff "
 
@@ -36,8 +43,23 @@ def translate(sensor_val, in_from, in_to, out_from, out_to):
     in_val = sensor_val - in_from
     val=(float(in_val)/in_range)*out_range
     out_val = out_from+val
-    return int(out_val)
+    rounded = int(round(out_val, -1))
+    return rounded
 
+def get_CPU_temp():
+    #Let's assme for now the logic to get CPU temps is valid
+    T1_command="sensors -Aj coretemp-isa-0000 | jq '.[][] | to_entries[] | select(.key | endswith(\"input\")) | .value' | sort -rn | head -n1"
+    T2_command="sensors -Aj coretemp-isa-0001 | jq '.[][] | to_entries[] | select(.key | endswith(\"input\")) | .value' | sort -rn | head -n1"
+
+    #run subprocess commands to get temps
+    temperature_1 = subprocess.check_output(T1_command, shell=True).decode('utf-8')
+    temperature_2 = subprocess.check_output(T2_command, shell=True).decode('utf-8')
+
+    logging.info("CPU1 Temp: " + temperature_1)
+    logging.info("CPU2 Temp: " + temperature_2)
+    average_temp = (int(temperature_1) + int(temperature_2)) / 2
+    logging.info("Average Temp: " + str(average_temp))
+    return average_temp
 
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
@@ -55,18 +77,7 @@ if __name__ == "__main__":
         exit(1)
 
 
-    #Let's assme for now the logic to get CPU temps is valid
-    T1_command="sensors -Aj coretemp-isa-0000 | jq '.[][] | to_entries[] | select(.key | endswith(\"input\")) | .value' | sort -rn | head -n1"
-    T2_command="sensors -Aj coretemp-isa-0001 | jq '.[][] | to_entries[] | select(.key | endswith(\"input\")) | .value' | sort -rn | head -n1"
-
-    #run subprocess commands to get temps
-    temperature_1 = subprocess.check_output(T1_command, shell=True).decode('utf-8')
-    temperature_2 = subprocess.check_output(T2_command, shell=True).decode('utf-8')
-
-    logging.info("CPU1 Temp: " + temperature_1)
-    logging.info("CPU2 Temp: " + temperature_2)
-    average_temp = (int(temperature_1) + int(temperature_2)) / 2
-    logging.info("Average Temp: " + str(average_temp))
+    
 
     #command to get ambient temp
     AMBIENT_command="ipmitool sdr type temperature | grep -i inlet | grep -Po '\d{2,3} degrees C' | grep -Po '\d{2,3}'"
@@ -80,6 +91,7 @@ if __name__ == "__main__":
         print ("#############################################")
         print ("[1] Test setting fan speed")
         print ("[2] Simulate temperature change and see how fan reacts")
+        print ("[3] Test script base don real temperature")
         print ("[0] Exit")
         print ("#############################################")
 
@@ -104,7 +116,7 @@ if __name__ == "__main__":
                 setFanSpeed(fan_speed)
 
             except ValueError:
-                print("You entered not valid number")
+                logging.error("You entered not valid number")
                 
         elif user_input == 2:
             logging.info("Simulating CPU Temperature Change")
@@ -119,16 +131,41 @@ if __name__ == "__main__":
                     input_temp = int(input_temp)
                     if input_temp == 0:
                         break
-                    elif input_temp <= 30:
+                    elif input_temp <= MIN_TEMP:
                         setFanSpeed(10)
-                    elif input_temp >= 80:
+                    elif input_temp >= MAX_TEMP:
                         setFanSpeed(100)
                     else:
                         #map input_temp to range of fan speed
                         #input_temp = 30-80
                         #fan_speed = 10-100
-                        fan_speed = translate(input_temp, 30, 80, 10, 100)
+                        fan_speed = translate(input_temp, MIN_TEMP, MAX_TEMP, MINIMAL_FAN_SPEED, MAXIMAL_FAN_SPEED)
                         setFanSpeed(fan_speed)
                 except ValueError:
-                    print("You entered not valid number")
+                    logging.error("You entered not valid number")
+
+        elif user_input == 3:
+            while True:
+                logging.info("Press Ctrl+C to exit")   
+                input_temp = get_CPU_temp() 
+                try:
+                    input_temp = int(input_temp)
+                    if input_temp == 0:
+                        break
+                    elif input_temp <= MIN_TEMP:
+                        setFanSpeed(10)
+                    elif input_temp >= MAX_TEMP:
+                        setFanSpeed(100)
+                    else:
+                        #map input_temp to range of fan speed
+                        #input_temp = 30-80
+                        #fan_speed = 10-100
+                        fan_speed = translate(input_temp, MIN_TEMP, MAX_TEMP, MINIMAL_FAN_SPEED, MAXIMAL_FAN_SPEED)
+                        setFanSpeed(fan_speed)
+                except ValueError:
+                    logging.error("Wrong CPU temp readed")
+                logging.info("Sleep for 60 seconds")
+                time.sleep(60)
+                
+
 
